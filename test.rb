05e5a3dce3fc40c3dd41e091912ef182bc4c0c9c
Igor Zubkov <igor.zubkov@gmail.com>
2019-04-25 23:07:21 +0300

    One more

--- test.rb
+++ test.rb
@@ -2,7 +2,7 @@
 
 output = `bundle exec rubocop`
 
-expected = '11 files inspected, 8 offenses detected'
+expected = '12 files inspected, 9 offenses detected'
 
 actual = output.split("\n").last
 
---
+++ tests/style/symbol_array/test.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+%i[foo bar baz]
