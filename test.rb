3c24c5e4645ec85a11008e50e8192053c155e0d0
Igor Zubkov <igor.zubkov@gmail.com>
2019-04-25 21:34:41 +0300

    One more

--- test.rb
+++ test.rb
@@ -2,7 +2,7 @@
 
 output = `bundle exec rubocop`
 
-expected = '6 files inspected, 4 offenses detected'
+expected = '7 files inspected, 5 offenses detected'
 
 actual = output.split("\n").last
 
---
+++ tests/gemspec/required_ruby_version/test.gemspec
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+Gem::Specification.new do |spec|
+  spec.required_ruby_version = '>= 2.2.0'
+end
