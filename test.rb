56207de09378f042c6d2fd23dab5f7d9bf0ad126
Igor Zubkov <igor.zubkov@gmail.com>
2019-04-29 15:22:24 +0300

    One more

--- test.rb
+++ test.rb
@@ -2,7 +2,7 @@
 
 output = `bundle exec rubocop`
 
-expected = '15 files inspected, 12 offenses detected'
+expected = '16 files inspected, 12 offenses detected'
 
 actual = output.split("\n").last
 
